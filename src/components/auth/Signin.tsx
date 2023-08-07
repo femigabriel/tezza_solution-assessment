695108fcb094370b1117046143bac35b7f7cfb6f
Femi Gabriel <timfemi@gmail.com>
2023-08-07 22:43:39 +0100

    login page fixesn

--- src/components/auth/Signin.tsx
+++ src/components/auth/Signin.tsx
@@ -1,19 +1,18 @@
 import React from "react";
-import { Button, Checkbox, Form, Input } from "antd";
 import SIgnInForm from "./SigninForm";
-const logo = "./assets/images/logo.webp";
+const logo = "./assets/images/depositphotos.jpg";
 
 export const Signin = () => {
   return (
-    <div className="flex justify-center items-center two w-full pt-32">
-      <div className="bg-white shadow-sm p-5 px-10 pt-10 w-full max-w-[450px] h-full">
+    <div className="flex justify-center items-center login w-full h-screen pt-32">
+      <div className="bg-white p-5 px-10 pt-10 w-full max-w-[450px] h-[] shadow-md">
         <div className="flex justify-between mb-5">
-          <div>
+          <div className="mt-5">
             <h3 className="text-[16px] font-semibold">Login</h3>
             <p>Login to your account</p>
           </div>
           <div>
-            <img src={logo} className="w-[60px] h-[60px]" alt="logo" />
+            <img src={logo} className="w-[95px] h-[95px] rounded-full" alt="logo" />
           </div>
         </div>
         <div>
